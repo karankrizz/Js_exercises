be9e47b3862116f476a404b62ba11b35c2bb314f
karankrizz <kkriz448@gmail.com>
2026-04-28 22:14:44 +0530

    worked on var

---
+++ A_variables/a_functionscope.js
@@ -0,0 +1,14 @@
+var bottle = "water ";
+var bottle = "milk";
+var bottle = "sugar";
+var bottle = "coffee powder";
+
+console.log("bottle====>", bottle);
+var bottle = "water ";
+var bottle = "milk";
+var bottle = "sugar";
+var bottle = "coffee powder";
+
+bottle = "chilli powder";
+bottle = "mouth-wash";
+console.log("bottle====>", bottle);
