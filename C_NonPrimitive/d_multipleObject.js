5e6e90706488b6d53149cc947a8f004bfb262af9
karankrizz <kkriz448@gmail.com>
2026-04-29 01:05:38 +0530

    worked on multiple objects

---
+++ C_NonPrimitive/d_multipleObject.js
@@ -0,0 +1,29 @@
+const employees = [
+    {
+        name : "Kirubhakaran",
+        age : "25",
+        location : "chennai",
+        isWorking : true,
+        salary : 152454.25,
+        skillset : ["Java","python","react","angular"]
+    },
+
+    {
+        name : "vignesh",
+        age : "31",
+        location : "chennai",
+        isWorking : true,
+        salary : 130002.25,
+        skillset : ["html","css","bs"]
+    },
+    {
+        name : "birla",
+        age : "26",
+        location : "chennai",
+        isWorking : true,
+        salary : 130002.25,
+        skillset : ["html","css","bs",".net","devops"]
+    }
+]
+
+console.log("employees------>",employees[1].salary)
