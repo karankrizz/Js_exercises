cdc4909be5c709895db27c8228ecc4463f0d5578
karankrizz <kkriz448@gmail.com>
2026-04-29 02:12:25 +0530

    worked on type of operator

---
+++ D_operators/i_typeofOperator.js
@@ -0,0 +1,11 @@
+let personname = "kirubha";
+let age = 25 ;
+let workingstatus = false;
+
+console.log("person name type ===>",typeof personname )
+console.log("person  age type ===>",typeof age )
+console.log("person  status type ===>",typeof workingstatus)
+
+
+
+
