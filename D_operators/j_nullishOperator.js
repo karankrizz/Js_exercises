c7b0e25d7672771cc719c1ced97152d15392daa1
karankrizz <kkriz448@gmail.com>
2026-04-29 02:25:03 +0530

    worked on nullish operator

---
+++ D_operators/j_nullishOperator.js
@@ -0,0 +1,6 @@
+var defaultbowler = null ?? "Bumrah";
+
+console.log("bowler Name ====>",defaultbowler)
+console.log("bowler Name length====>",defaultbowler.length)
+console.log("bowler Name uppercase====>",defaultbowler.toUpperCase())
+console.log("bowler Name lowercase===>", defaultbowler.toLowerCase())
