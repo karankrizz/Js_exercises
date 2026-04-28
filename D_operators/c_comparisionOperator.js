d10117b4adc4e054e663edad3e269201344aca1a
karankrizz <kkriz448@gmail.com>
2026-04-29 01:43:43 +0530

    worked on comparision

--- D_operators/c_comparisionOperator.js
+++ D_operators/c_comparisionOperator.js
@@ -1,17 +1,16 @@
 const firstValue = 200;
 const secondValue = 500;
 
-console.log("Checking Greater then ===>",(firstValue > secondValue) )         
+console.log("Checking Greater then ===>", (firstValue > secondValue))
 
-console.log("Checking Less then ===>",(firstValue < secondValue) ) 
-           
-console.log("Checking Less then Equal to ===>",(firstValue <= secondValue) ) 
+console.log("Checking Less then ===>", (firstValue < secondValue))
 
-console.log("Checking greater then Equal to ===>",(firstValue >= secondValue) ) 
+console.log("Checking Less then Equal to ===>", (firstValue <= secondValue))
 
-console.log("Equal to  ===>",(firstValue == secondValue) )                     
+console.log("Checking greater then Equal to ===>", (firstValue >= secondValue))
 
-console.log("Strict equal to  ===>",(firstValue === secondValue) )            
+console.log("Equal to  ===>", (firstValue == secondValue))
+
+console.log("Strict equal to  ===>", (firstValue === secondValue))
 
 
- 
