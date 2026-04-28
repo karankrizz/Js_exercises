4c07447f841d196498f08660a9bad3eb0b7f15d3
karankrizz <kkriz448@gmail.com>
2026-04-29 01:15:18 +0530

    worked on assignment operator

---
+++ D_operators/b_assginmentOperator.js
@@ -0,0 +1,18 @@
+let x = 100;
+
+
+/* addition assignment */
+x +=200
+console.log("addition ===>",x) //300
+
+/* subraction assignment */
+x -=200
+console.log("subtraction===>", x)//100
+/* Multiplication assignment */
+x *=100
+console.log("multiplication===>",x)//10000
+/* division assignment */
+x /=0
+console.log("division===>", x)// Infinity
+
+
