dbb8d01f27cf1454455978a8fc0e27e9d20b4102
karankrizz <kkriz448@gmail.com>
2026-04-28 22:15:22 +0530

    worked on string

---
+++ B_primitiveDataType/a_stringType.js
@@ -0,0 +1,14 @@
+let candidateFirstName = "kirubha";
+let candidateLastName = "karan";
+let initial = "R";
+
+console.log(
+  "Fullname==>",
+  candidateFirstName + "" + candidateLastName + " " + initial,
+);
+console.log("initial==>", initial);
+
+let firstNumber = "1000";
+let secondNumber = "2000";
+
+console.log("total==>", typeof firstNumber +secondNumber)
