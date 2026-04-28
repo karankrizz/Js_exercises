0c20077359cee8b9d21b908fb2042518cfa2d429
karankrizz <kkriz448@gmail.com>
2026-04-29 01:43:57 +0530

    worked on logical operator

---
+++ D_operators/e_logicalOperator.js
@@ -0,0 +1,38 @@
+
+var x = 100
+var y = 100
+
+var a = 500
+var b = 600
+var r= 50
+var k=50
+/* Example : AND */
+console.log((x == y) && (a == b))                 
+console.log( (x==y) && (r==k))
+
+/*  Example : OR */
+
+console.log((x == y) || (a == b))               
+
+/* Example : Not (!) */
+
+var productAvailable = false;
+var product = "apple"
+var productprice = 100;
+
+var productOffer = !productAvailable ? "stock empty" : productprice * 5 / 100;
+
+console.log("productOffer===>", productOffer)
+
+var productAvailable = true;
+var product = "apple"
+var productprice = 100;
+
+var productOffer = !productAvailable ? "stock empty" : productprice * 5 / 100;
+
+console.log("productOffer===>", productOffer)
+
+
+
+
+
