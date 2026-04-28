8ad28c01b17b9e871cf2a18dadd6a55cf4e5c7a1
karankrizz <kkriz448@gmail.com>
2026-04-28 22:20:30 +0530

    worked on numbertype

---
+++ B_primitiveDataType/b_numberType.js
@@ -0,0 +1,12 @@
+
+
+let principalAmount = 150000;
+let numberofyears = 2 
+let intrestPerncentage = 4.5;    
+
+let simpleInterest  = principalAmount * numberofyears * intrestPerncentage / 100;
+let totalAmount = simpleInterest + principalAmount
+
+
+console.log("simpleinterest ====>", simpleInterest)
+console.log("totalAmount ====>", totalAmount)
