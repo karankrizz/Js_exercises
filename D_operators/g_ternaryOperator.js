db3a5d5698e750c8ad655f4d12f8b855df66fc60
karankrizz <kkriz448@gmail.com>
2026-04-29 01:56:03 +0530

    worked on ternary operator

---
+++ D_operators/g_ternaryOperator.js
@@ -0,0 +1,11 @@
+
+
+var age = 53;
+
+var result = (age >= 18) ? "adult" : "minor";
+console.log(result)
+//---------------------------------//
+var age = 13;
+
+var result = (age >= 18) ? "adult" : "minor";
+console.log(result)
