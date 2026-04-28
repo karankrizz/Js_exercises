05d6a93ddf0c4e7f99c8c2a43dc59f98574c71c2
karankrizz <kkriz448@gmail.com>
2026-04-29 02:42:57 +0530

    worked on explicit

---
+++ E_TypeConversation/b_Explicit.js
@@ -0,0 +1,45 @@
+
+/* Example 1 : String to Number */
+
+var firstNumber = "619"
+var secondNumber = "786"
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+var result  = firstNumber + secondNumber;  
+console.log("number added ===>",result)  
+
+/* Example 2 : Number to String */
+
+
+var firstNumber = 600
+var secondNumber = 5160
+
+firstNumber = String(firstNumber)
+secondNumber = String(secondNumber)
+
+var result  = firstNumber + secondNumber;  
+
+console.log(result)
+
+var firstNumber = 600
+var secondNumber = 5160
+var result  = String(firstNumber) + secondNumber; 
+console.log(result)
+
+/* Example 3 : Boolean conversation */
+
+var productAvailable = 1
+var result = Boolean(productAvailable)
+
+
+console.log(result)
+
+var productAvailable = 0
+var result = Boolean(productAvailable)
+
+
+console.log(result)
+
+
