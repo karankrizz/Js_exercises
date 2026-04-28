03cb07b26cdc2c3642c5dec6b20a6dcbb4c75947
karankrizz <kkriz448@gmail.com>
2026-04-29 03:01:24 +0530

    worked on elseif lader

---
+++ A_conditonStatement/c_elseifLader.js
@@ -0,0 +1,36 @@
+  var marks = 35;
+
+    if (marks >= 45) {
+        console.log("well passed");
+    }
+     else if (marks >= 35) {
+        console.log("just passed");
+    } 
+    else {
+        console.log("fail");
+    }
+
+    //--------//
+      var marks = 25;
+
+    if (marks >= 45) {
+        console.log("well passed");
+    }
+     else if (marks >= 35) {
+        console.log("just passed");
+    } 
+    else {
+        console.log("fail");
+    }
+    //------//
+      var marks = 55;
+
+    if (marks >= 45) {
+        console.log("well passed");
+    }
+     else if (marks >= 35) {
+        console.log("just passed");
+    } 
+    else {
+        console.log("fail");
+    }
