fd050dfbeebcade98681ccf16d9946caa3d5722a
karankrizz <kkriz448@gmail.com>
2026-04-29 11:32:28 +0530

    made the format of text

--- D_operators/g_ternaryOperator.js
+++ D_operators/g_ternaryOperator.js
@@ -2,10 +2,10 @@
 
 var age = 53;
 
-var result = (age >= 18) ? "adult" : "minor";
+var result = (age >= 18) ? "Adult" : "Minor";
 console.log(result)
 //---------------------------------//
 var age = 13;
 
-var result = (age >= 18) ? "adult" : "minor";
+var result = (age >= 18) ? "Adult" : "Minor";
 console.log(result)
