d107023f1dc1c49db5d071f5be49fd98f048c78a
karankrizz <kkriz448@gmail.com>
2026-04-29 23:38:44 +0530

    worked on for loop

---
+++ B_jsLoops/a_forLoop.js
@@ -0,0 +1,35 @@
+/* Example 1 */
+
+for (let i = 9; i <=18; i++) {
+
+  // console.log("i===>", i)
+
+}
+
+
+
+/* Example 2 */
+for (let i = 9; i <18 ;i++) {
+
+ // console.log("i==>", i)
+
+}
+
+
+
+/* Example  3*/
+
+let icecreams = ["vennila ", "chocolate", "blackcurrant", "butterscotch"];
+
+for (let i = 0; i <2 ; i++) {
+ // console.log("icecreams===>",icecreams[i])
+}
+
+
+/* Example  4*/
+
+let flavour = ["vennila ", "chocolate", "blackcurrant", "butterscotch"];
+
+for (let i = 0; i <flavour.length ; i++) {
+  console.log("flavour===>",flavour[i])
+}
