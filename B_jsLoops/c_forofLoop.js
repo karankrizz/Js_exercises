3762c1ef66d0f807d79689278da3cd1f560867b3
karankrizz <kkriz448@gmail.com>
2026-04-29 23:42:09 +0530

    worked in forof loop

---
+++ B_jsLoops/c_forofLoop.js
@@ -0,0 +1,20 @@
+let numbers = [180, 220, 330];
+
+for (let item of numbers) {
+   // console.log(item);
+}
+
+
+
+
+
+
+let text = "hello kirubha"
+
+
+for (let item of text) {
+
+     console.log(item)            
+
+}
+
