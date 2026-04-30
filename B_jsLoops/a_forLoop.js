6636714b7eb39d8a203fcbd9bb4768f0769ed6cf
karankrizz <kkriz448@gmail.com>
2026-04-30 09:59:40 +0530

    worked out for loops

--- B_jsLoops/a_forLoop.js
+++ B_jsLoops/a_forLoop.js
@@ -31,5 +31,9 @@ for (let i = 0; i <2 ; i++) {
 let flavour = ["vennila ", "chocolate", "blackcurrant", "butterscotch"];
 
 for (let i = 0; i <flavour.length ; i++) {
-  console.log("flavour===>",flavour[i])
+  //console.log("flavour===>",flavour[i])
+}
+/* Example 5 tables */
+for (let i=11 ; i< 20 ;i++){
+  console.log("tables==>",`${i} * 21 = ${i*21}`)
 }
