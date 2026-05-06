4aa97dd78fcc0fec7b0c70c4fb292c678876ebe5
karankrizz <kkriz448@gmail.com>
2026-05-07 00:19:10 +0530

    learnt array remove in initial and last

---
+++ A_Array-Functions/b_removeElement.js
@@ -0,0 +1,7 @@
+let lastremove=["vijay","stalin","Edapati palanisamy"]
+lastremove.pop()
+console.log("lastremove===>",lastremove)
+
+let firstremove=["vijay","stalin","Edapati palanisamy"]
+firstremove.shift()
+console.log("firstremove===>",firstremove)
