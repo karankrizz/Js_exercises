9219971001ae43948b77f5a129c740da69fcaf57
karankrizz <kkriz448@gmail.com>
2026-05-07 00:14:52 +0530

    learned array insert in initial and last

---
+++ A_Array-Functions/a_arrayInsert.js
@@ -0,0 +1,13 @@
+let lastaddcm = ["vijay","M.K.Stalin","Edapati palanisamy"]
+lastaddcm.push("Sivakarthikeyan")
+lastaddcm.push("Pradeep Ranganathan")
+
+console.log("lastaddcm===>",lastaddcm)
+
+let firstaddcm = ["vijay","M.K.Stalin","Edapati palanisamy"]
+firstaddcm.unshift("Sivakarthikeyan")
+firstaddcm.unshift("Pradeep Ranganathan")
+
+console.log("firstaddcm===>",firstaddcm)
+
+
