fec17796c70638899a751cfa1d2218ec87c4ba02
karankrizz <kkriz448@gmail.com>
2026-06-01 18:00:31 +0530

    refactor formatting and improve string concatenation in a_stringType.js

--- B_primitiveDataType/a_stringType.js
+++ B_primitiveDataType/a_stringType.js
@@ -1,14 +1,18 @@
+//Strings Explanation
+
 let candidateFirstName = "kirubha";
 let candidateLastName = "karan";
 let initial = "R";
 
 console.log(
   "Fullname==>",
-  candidateFirstName + "" + candidateLastName + " " + initial,
-);
+  candidateFirstName+candidateLastName + " " + initial,
+)
 console.log("initial==>", initial);
+
+//BETTER UNDERSTANDING ABOUT THE STRINGS VS NUMBER DATATYPE
 
 let firstNumber = "1000";
 let secondNumber = "2000";
 
-console.log("total==>", typeof firstNumber +secondNumber)
+console.log("total==>", typeof firstNumber + secondNumber)
