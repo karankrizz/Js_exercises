8413be790c2ace95b2b65aea96a407b568ef01de
karankrizz <kkriz448@gmail.com>
2026-06-01 18:08:31 +0530

    refactor array formatting and improve console log spacing in A_Array.js

--- C_NonPrimitive/A_Array.js
+++ C_NonPrimitive/A_Array.js
@@ -2,8 +2,8 @@
  *  Array => an array is a group of related data item.
  */
 
-let Actors = ["ajith","vijay","rajini","kamal","sivakarthikeyan"]
-let salaryBasic = [50000,60000,70000,90000,80000]
+let Actors = ["ajith", "vijay", "rajini", "kamal", "sivakarthikeyan"]
+let salaryBasic = [50000, 60000, 70000, 90000, 80000]
 console.log("actors===>", Actors[1])
 
 
@@ -12,11 +12,11 @@ console.log("actors===>", Actors[1])
  *  List => A list is a group of un-related data item.
  */
 
-let personDetails = ["kirubhakaran",25,"Msc maths",52000.25,]
+let personDetails = ["kirubhakaran", 25, "Msc maths", 52000.25,]
 
 
-console.log("personDetails====>",personDetails)
-console.log("person qualification ====>",personDetails[2])
+console.log("personDetails====>", personDetails)
+console.log("person qualification ====>", personDetails[2])
 console.log("personage===>", personDetails[1])
 console.log("personname===>", personDetails[0])
 console.log("personsalary===>", personDetails[3])
