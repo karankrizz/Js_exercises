ac951e50c15a25b1dcde534a41ebd2c797a55681
karankrizz <kkriz448@gmail.com>
2026-06-10 22:59:14 +0530

    Fix formatting and spacing in c_filter.js for improved readability

--- C_HOF/c_filter.js
+++ C_HOF/c_filter.js
@@ -1,24 +1,25 @@
-let orignalArr = [100,200,300,400,750,1000]
+let orignalArr = [100, 200, 300, 400, 750, 1000]
 
 let greaterThen500 = orignalArr.filter(
-    (value,index,arr) => {
+    (value, index, arr) => {
         return value > 500
     }
 )
-console.log("500 greater then ====>",greaterThen500)
+console.log("500 greater then ====>", greaterThen500)
 
 /* example 2 - Remove duplicate Elements */
 
-let array = [5,5,6,100,100,10,5,9,6]
+let array = ["apple", "orange", "apple", "mango", "mango", "pineapple", "grapes", "grapes", "lichi", "pineapple"]
 
 let uniqueArr = array.filter(
-    (value,index,arr) =>{
+    (value, index, arr) => {
 
-        return arr.indexOf(value) == index         // 2 == 2      5
+        return arr.indexOf(value) == index
+        // 
     }
 )
 
-console.log("unique Arrr ====>",uniqueArr)
+console.log("unique Arrr ====>", uniqueArr)
 
 
 
